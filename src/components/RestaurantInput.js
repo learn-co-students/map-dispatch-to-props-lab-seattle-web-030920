b7a1199c8e73d4d4cc11f2080ad4197885754d96
github username <github email address>
2020-06-01 12:12:21 -0700

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -3,7 +3,6 @@ import { addRestaurant } from '../actions/restaurants';
 import { connect } from 'react-redux';
 
 export class RestaurantInput extends Component {
-
   state = {
     name: '',
     location: ''
@@ -23,7 +22,11 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
-    // add missing code
+    this.props.addRestaurant(this.state)
+    this.setState({
+      name: '',
+      location: ''
+    })
   }
 
   render() {
@@ -49,6 +52,9 @@ export class RestaurantInput extends Component {
   }
 };
 
+// const mapDispatchToProps = dispatch => {
+//   return { addRestaurant: () => dispatch(addRestaurant()) }
+// }
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(null, { addRestaurant })(RestaurantInput)
